3e6fb071b94aced2216cc4493c64a07a0b1e6ca6
Xinyu <157100455@qq.com>
2016-01-02 14:25:20 +0800

    Add baidu data analysis

--- header.php
+++ header.php
@@ -3,24 +3,32 @@
 <head>
     <meta charset="<?php $this->options->charset(); ?>" />
     <meta http-equiv="X-UA-Compatible" content="IE=Edge,chrome=1">
-	<meta name="viewport" content="width=device-width,user-scalable=no">
+    <meta name="viewport" content="width=device-width,user-scalable=no">
     <title><?php $this->archiveTitle('', '', ' - '); ?><?php $this->options->title(); ?></title>
-	<!--[if lt IE 9]>
+    <!--[if lt IE 9]>
         <script type="text/javascript" src="<?php $this->options->themeUrl('javascript/html5shiv.js'); ?>"></script>
     <![endif]-->
     <meta name ="title" content="iwxy.me">
     <meta name ="keywords" content="iwxy.me,SwimInSky">
+    <meta name="apple-mobile-web-app-capable" content="yes" />
+    <meta name="apple-mobile-web-app-status-bar-style" content="white">
     <meta name ="description" content="iwxy.me　是一个以学习和分享为目的的原创写作博客，技术方向为前端开发，分享经验，记录点滴">
     <link rel="stylesheet" href="<?php $this->options->adminUrl('css/normalize.css'); ?>">
     <link rel="stylesheet" href="<?php $this->options->themeUrl('style.min.css'); ?>">
-    <!-- 开启iphone下WebApp支持 -->
-    <meta name="apple-mobile-web-app-capable" content="yes" />
-    <meta name="apple-mobile-web-app-status-bar-style" content="white">
     <link rel="shortcut icon" href="<?php $this->options->themeUrl('icon.jpg'); ?>" type="image/x-icon">
     <link rel="apple-touch-icon-precomposed" href="<?php $this->options->themeUrl('icon.jpg'); ?>" />
     <script type="text/javascript" src="<?php $this->options->themeUrl('javascript/jquery.min.js'); ?>"></script>
     <?php $this->header('keywords=&description=&generator=&template=&pingback=&xmlrpc=&wlw=&rss1=&rss2&atom='); ?>
-
+    <script>
+    //Data Analysis
+    var _hmt = _hmt || [];
+    (function() {
+      var hm = document.createElement("script");
+      hm.src = "//hm.baidu.com/hm.js?bdedf6ddd111350117359d07619ab1c2";
+      var s = document.getElementsByTagName("script")[0]; 
+      s.parentNode.insertBefore(hm, s);
+    })();
+    </script>
 </head>
 <body>
 <div id="particles-js">
@@ -39,16 +47,16 @@
                         <?php $this->options->title() ?>
                     </a>
                 <?php endif; ?>
-        	    <p class="description"><?php $this->options->description() ?></p>
+                <p class="description"><?php $this->options->description() ?></p>
             </div>
             <div>
                 <nav id="nav-menu" class="clearfix">
                     <a class="<?php if($this->is('index')): ?>current<?php endif; ?> <?php if($this->is('post')): ?>current<?php endif; ?>" href="<?php $this->options->siteUrl(); ?>"><?php _e('文章'); ?></a>
-    				<?php $this->widget('Widget_Contents_Page_List')->to($pages); ?>
-    				<?php while($pages->next()): ?>
-    				<a <?php if($this->is('page', $pages->slug)): ?> class="current"<?php endif; ?> href="<?php $pages->permalink(); ?>" title="<?php $pages->title(); ?>"><?php $pages->title(); ?></a>
-    				<?php endwhile; ?>
-    			</nav>
+                    <?php $this->widget('Widget_Contents_Page_List')->to($pages); ?>
+                    <?php while($pages->next()): ?>
+                    <a <?php if($this->is('page', $pages->slug)): ?> class="current"<?php endif; ?> href="<?php $pages->permalink(); ?>" title="<?php $pages->title(); ?>"><?php $pages->title(); ?></a>
+                    <?php endwhile; ?>
+                </nav>
             </div>
         </div>
     </div>
