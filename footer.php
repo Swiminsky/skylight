f291b2531a824c889d5b0a1c589b1ae248f1455e
Xinyu <157100455@qq.com>
2016-01-04 17:41:38 +0800

    update the footer

--- footer.php
+++ footer.php
@@ -4,11 +4,12 @@
 <footer id="footer">
 	<div class="container">
 		&copy; <?php echo date('Y'); ?> <a rel="nofollow" href="<?php $this->options->siteUrl(); ?>"><?php $this->options->title(); ?></a>
+		<br>	
+  			Themed by <a rel="nofollow" href="<?php $this->options->siteUrl(); ?>"><?php $this->options->title(); ?></a> . Powered by <a rel="nofollow" href="http://typecho.org/">Typecho</a>
 	</div>
 </footer>
 </div>
 <script src="<?php $this->options->themeUrl('javascript/particles.min.js'); ?>"></script>
 <script src="<?php $this->options->themeUrl('javascript/app.js'); ?>"></script>
-<?php $this->footer(); ?>
 </body>
 </html>
