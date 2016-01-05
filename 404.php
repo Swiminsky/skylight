728eaefc2b181656ea41b8ef433ba55f56a1ae0a
Xinyu <157100455@qq.com>
2016-01-05 12:26:15 +0800

    update structure and footer

--- 404.php
+++ 404.php
@@ -1,8 +1,8 @@
 <!DOCTYPE HTML>
-<html class="no-js">
+<html class="no-js" lang="zh-CN">
 <head>
   <meta charset="<?php $this->options->charset(); ?>" />
-	<meta name="viewport" content="width=device-width,user-scalable=no">
+  <meta name="viewport" content="width=device-width,user-scalable=no,initial-scale=1.0">
   <?php if ($this->is('index')): ?><title></title>
 	<?php else: ?>
 	<title><?php $this->archiveTitle('.', '', ' - '); ?>SwimInSky</title>
@@ -10,7 +10,6 @@
 	<!--[if lt IE 9]>
     <link rel="stylesheet" href="<?php $this->options->themeUrl('javascript/html5shiv.js'); ?>">
   <![endif]-->
-  <link rel="stylesheet" href="<?php $this->options->adminUrl('css/normalize.css'); ?>">
   <link rel="stylesheet" href="<?php $this->options->themeUrl('style.min.css'); ?>">
   <?php $this->header('keywords=&description=&generator=&template=&pingback=&xmlrpc=&wlw=&rss1=&rss2&atom='); ?>
 </head>
@@ -18,19 +17,16 @@
 <div id="particles-js">
   <div class="body404">
     <div class="info404">
-      <header id="header404">
-        <div class="site-name404">
-          <i>404</i>
-        </div>
-      </header>
+      <header>404</header>
       <section>
-        <div class="title404"><p>抱歉，这个页面找不到了</p></div>
-        <a class="index404" rel="nofollow" href="<?php $this->options->siteUrl(); ?>">回首页看看</a>
+        <div class="text404">抱歉，这个页面找不到了</div>
+        <a class="back404" rel="nofollow" href="<?php $this->options->siteUrl(); ?>">回首页看看</a>
       </section>
-      <footer id="footer404">
-        &copy; <?php echo date('Y'); ?> <?php $this->options->title(); ?></span>
-      </footer>
     </div>
+      <footer id="footer404">
+        &copy; <?php echo date('Y'); ?> <a rel="nofollow" href="<?php $this->options->siteUrl(); ?>"><?php $this->options->title(); ?></a><br>  
+        Themed by <a rel="nofollow" href="<?php $this->options->siteUrl(); ?>"><?php $this->options->title(); ?></a> . Powered by <a rel="nofollow" href="http://typecho.org/">Typecho</a>
+      </footer>
   </div>
 </div>
 <script src="<?php $this->options->themeUrl('javascript/particles.min.js'); ?>"></script>
